326aee99bf7cb19be9f3945eaccff9a0d6d98fe9
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2022-11-13 20:34:02 +0100

    Update types to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './i18n';
-import App from './components/App';
-import { AuthProvider } from './context/AuthContext';
-import { AppProvider } from './context/AppContext/AppContext';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { HelmetProvider } from 'react-helmet-async';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./i18n";
+import App from "./components/App";
+import { AuthProvider } from "./context/AuthContext";
+import { AppProvider } from "./context/AppContext/AppContext";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { HelmetProvider } from "react-helmet-async";
 
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
-
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <HelmetProvider>
       <AuthProvider>
         <AppProvider>
           <App />
         </AppProvider>
-      </AuthProvider>  
+      </AuthProvider>
     </HelmetProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
