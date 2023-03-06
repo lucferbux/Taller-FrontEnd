2a872d59f5b58635b20c5a8a176c7335a8bdc73d
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2023-03-06 17:36:57 +0100

    Fix env variable import

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,5 @@
-/*
- * Route configuration:
- * If BASE_URL is defined (via ENV var or via HTML-embed) the API_BASE_URI and SIMULATOR_BASE_URI are created by appending
- * If it is not defined (dev environment), it will read the ENV vars: REACT_APP_API_URI and REACT_APP_SIMLATOR_URI
- */
-const baseUrl = process.env.REACT_APP_BASE_URI;
-let apiBaseUrl = process.env.REACT_APP_API_URI;
+const baseUrl = import.meta.env.VITE_BASE_URI;
+let apiBaseUrl = import.meta.env.VITE_API_URI;
 
 if (baseUrl) {
   apiBaseUrl = baseUrl + "/_/api";
