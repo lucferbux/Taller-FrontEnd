655aebf058201fc4234ec06716ad9f40a30fd096
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2023-09-09 18:26:19 +0200

    Add check for context

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,5 +4,9 @@ import AuthContext from '../context/AuthContext';
 export default function useAuth() {
   const context = useContext(AuthContext);
 
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
   return context;
 }
